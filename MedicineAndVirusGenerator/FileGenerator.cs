c107ed7d67b70ce5ccdd955cbd3f65c4c1bc3490
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2017-12-11 01:32:15 +0200

    Binary, Integer, 'K E S T A S' basic file generation implemented

--- MedicineAndVirusGenerator/FileGenerator.cs
+++ MedicineAndVirusGenerator/FileGenerator.cs
@@ -10,45 +10,112 @@ namespace MedicineAndVirusGenerator
     class FileGenerator
     {
         string _filename;
-        private static Random random = new Random();
+        private static Random _random = new Random();
 
         public FileGenerator(string filename)
         {
             _filename = filename;
         }
 
-        public void GenerateFile(int lines)
+        public void WriteToFile(string text)
         {
-            string generatedText = GenerateText(lines);
-
             using (StreamWriter sw = File.CreateText(_filename))
             {
-                sw.Write(generatedText);
+                sw.Write(text);
             }
         }
 
-        private string GenerateText(int lines)
+        public void KestasGenerate(int lines)
         {
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i<lines; i++)
+            for (int i = 0; i < lines; i++)
             {
                 sb.Append(RandomString(8));
-                for (int j = 0; j<10; j++)
+                for (int j = 0; j < 10; j++)
                 {
-                    string boolValue = random.Next(0, 2).ToString();
-
+                    string boolValue = _random.Next(0, 2).ToString();
                     sb.Append(" ").Append(boolValue);
                 }
                 sb.AppendLine();
             }
-            return sb.ToString();
+            string generatedText = sb.ToString();
+
+            WriteToFile(generatedText);
+        }
+
+        public void BinaryGenerate(int lines, int lineLength)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ///generate lines except last one
+            for (int i = 0; i < lines-1; i++)
+            {
+                ///generate last bool in line
+                for (int j = 0; j < lineLength-1; j++)
+                {
+                    string boolValue = _random.Next(0, 2).ToString();
+                    sb.Append(boolValue).Append(" ");
+                }
+                string lastBool = _random.Next(0, 2).ToString();
+                sb.Append(lastBool);
+                sb.AppendLine();
+            }
+            ///generate last line in file
+            for (int j = 0; j < lineLength - 1; j++)
+            {
+                string boolValue = _random.Next(0, 2).ToString();
+                sb.Append(boolValue).Append(" ");
+            }
+            ///generate last bool in file
+            string lastFileBool = _random.Next(0, 2).ToString();
+            sb.Append(lastFileBool);
+
+            string generatedText = sb.ToString();
+
+            WriteToFile(generatedText);
+        }
+
+        public void IntegerGenerate(int lines, int lineLength, int maxInteger)
+        {
+            maxInteger++;
+
+            StringBuilder sb = new StringBuilder();
+
+            ///generate lines except last one
+            for (int i = 0; i < lines - 1; i++)
+            {
+                ///generate last bool in line
+                for (int j = 0; j < lineLength - 1; j++)
+                {
+                    string intValue = _random.Next(0, maxInteger).ToString();
+                    sb.Append(intValue).Append(" ");
+                }
+                string lastInt = _random.Next(0, maxInteger).ToString();
+                sb.Append(lastInt);
+                sb.AppendLine();
+            }
+            ///generate last line in file
+            for (int j = 0; j < lineLength - 1; j++)
+            {
+                string intValue = _random.Next(0, maxInteger).ToString();
+                sb.Append(intValue).Append(" ");
+            }
+            ///generate last bool in file
+            string lastFileInt = _random.Next(0, maxInteger).ToString();
+            sb.Append(lastFileInt);
+
+            string generatedText = sb.ToString();
+
+            WriteToFile(generatedText);
         }
 
         public static string RandomString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
-              .Select(s => s[random.Next(s.Length)]).ToArray());
+              .Select(s => s[_random.Next(s.Length)]).ToArray());
         }
+
+
     }
 }
--- MedicineAndVirusGenerator/Program.cs
+++ MedicineAndVirusGenerator/Program.cs
@@ -15,36 +15,55 @@ namespace MedicineAndVirusGenerator
             Console.WriteLine("------ K e s t a s - B O T ------");
             Console.WriteLine("---------------------------------\n");
 #endregion
+            start:
             Console.WriteLine("> Iveskite sugeneruotu irasu kieki");
             int irasuKiekis = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("> Iveskite failo pavadinima (su formatu). Pvz: data.txt");
+            string failoPavadinimas = Console.ReadLine();
+
+            /* not yet implemented
+            Console.WriteLine("> Ar reikia issaugoti irasu skaiciu ir irasyti failo pradzioje. Iveskite 't' jei taip");
+            string kiekioIssaugojimas_string = Console.ReadLine();
+            bool kiekioIssaugojimas = (kiekioIssaugojimas_string == "t") ? true : false;
+            */
+
+            FileGenerator generatorius = new FileGenerator(failoPavadinimas);
 
             tipoPasirinkimas:
             Console.WriteLine("> Pasirinkite duomenu tipa");
             Console.WriteLine("> 1) Binary");
-            Console.WriteLine("> 2) Numbers (integers)");
+            Console.WriteLine("> 2) Numbers (integers) (a.k.a. Barbora's generation)");
             Console.WriteLine("> 69) The K E S T A S special");
 
             int pasirinkimas = int.Parse(Console.ReadLine());
             switch (pasirinkimas)
             {
                 case 1:
+                    Console.WriteLine("> Iveskite eilutes ilgi (t.y. kiek bool bus eilutej)");
+                    int ilgis_binary = int.Parse(Console.ReadLine());
+                    generatorius.BinaryGenerate(irasuKiekis, ilgis_binary);
                     break;
                 case 2:
+                    Console.WriteLine("> Iveskite eilutes ilgi (t.y. kiek bool bus eilutej)");
+                    int ilgis_int = int.Parse(Console.ReadLine());
+                    Console.WriteLine("> Iveskite max skaiciu (inclusive)");
+                    int maxSkaicius = int.Parse(Console.ReadLine());
+                    generatorius.IntegerGenerate(irasuKiekis, ilgis_int, maxSkaicius);
                     break;
                 case 69:
-                    FileGenerator virusuGeneratorius = new FileGenerator("Virusai.");
-                    virusuGeneratorius.GenerateFile(8);
-                    FileGenerator vaistuGeneratorius = new FileGenerator("Vaistai.");
-                    vaistuGeneratorius.GenerateFile(4);
+                    generatorius.KestasGenerate(irasuKiekis);
                     break;
                 default:
                     Console.WriteLine("> Klaida: Neteisingas pasirinkimas. Bandykite dar karta");
                     goto tipoPasirinkimas;
             }
 
-            //TODO:
-            //Console.WriteLine(">Iveskite max skaiciaus verte");
-            //int irasuKiekis = int.Parse(Console.ReadLine());
+            Console.WriteLine("> Failas sugeneruotas sekmingai!");
+            Console.WriteLine("> Kartoti? Iveskite 't'");
+            string kartoti = Console.ReadLine();
+            if (kartoti == "t")
+                goto start;
         }
     }
 }
