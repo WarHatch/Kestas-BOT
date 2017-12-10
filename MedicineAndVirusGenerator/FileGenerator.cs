dbf9361a999e22b036c247a7c0bd9d5a5a9af8c3
Karolis Staskevičius <satelistas@gmail.com>
2017-12-10 22:19:30 +0200

    Initial commit
    Committed program from my machine

---
+++ MedicineAndVirusGenerator/FileGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicineAndVirusGenerator
+{
+    class FileGenerator
+    {
+        string _filename;
+        private static Random random = new Random();
+
+        public FileGenerator(string filename)
+        {
+            _filename = filename;
+        }
+
+        public void GenerateFile(int lines)
+        {
+            string generatedText = GenerateText(lines);
+
+            using (StreamWriter sw = File.CreateText(_filename))
+            {
+                sw.Write(generatedText);
+            }
+        }
+
+        private string GenerateText(int lines)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i<lines; i++)
+            {
+                sb.Append(RandomString(8));
+                for (int j = 0; j<10; j++)
+                {
+                    string boolValue = random.Next(0, 2).ToString();
+
+                    sb.Append(" ").Append(boolValue);
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ MedicineAndVirusGenerator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicineAndVirusGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#region Intro
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("------ K e s t a s - B O T ------");
+            Console.WriteLine("---------------------------------\n");
+#endregion
+            Console.WriteLine("> Iveskite sugeneruotu irasu kieki");
+            int irasuKiekis = int.Parse(Console.ReadLine());
+
+            tipoPasirinkimas:
+            Console.WriteLine("> Pasirinkite duomenu tipa");
+            Console.WriteLine("> 1) Binary");
+            Console.WriteLine("> 2) Numbers (integers)");
+            Console.WriteLine("> 69) The K E S T A S special");
+
+            int pasirinkimas = int.Parse(Console.ReadLine());
+            switch (pasirinkimas)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 69:
+                    FileGenerator virusuGeneratorius = new FileGenerator("Virusai.");
+                    virusuGeneratorius.GenerateFile(8);
+                    FileGenerator vaistuGeneratorius = new FileGenerator("Vaistai.");
+                    vaistuGeneratorius.GenerateFile(4);
+                    break;
+                default:
+                    Console.WriteLine("> Klaida: Neteisingas pasirinkimas. Bandykite dar karta");
+                    goto tipoPasirinkimas;
+            }
+
+            //TODO:
+            //Console.WriteLine(">Iveskite max skaiciaus verte");
+            //int irasuKiekis = int.Parse(Console.ReadLine());
+        }
+    }
+}
